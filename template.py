d24604a5803ddb9477faa36ef7846d4f399df391
Badara Diallo <badaradiallo@Host-002.lan>
2025-07-16 16:41:59 +0200

    Project Structure

---
+++ template.py
@@ -0,0 +1,50 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format="[%(asctime)s]: %(message)s:")
+
+
+project_name = "books_recommender"
+
+
+list_of_files = [
+    f"{project_name}/__init__.py",
+    f"{project_name}/components/__init__.py",
+    f"{project_name}/components/stage_00_data_ingestion.py",
+    f"{project_name}/components/stage_01_data_validation.py",
+    f"{project_name}/components/stage_02_data_transformation.py",
+    f"{project_name}/components/stage_03_model_trainer.py",
+    f"{project_name}/config/__init__.py",
+    f"{project_name}/config/configuration.py",
+    f"{project_name}/constant/__init__.py",
+    f"{project_name}/entity/__init__.py",
+    f"{project_name}/entity/config_entity.py",
+    f"{project_name}/exception/__init__.py",
+    f"{project_name}/exception/exception_handler.py",
+    f"{project_name}/logger/__init__.py",
+    f"{project_name}/logger/log.py",
+    f"{project_name}/pipeline/__init__.py",
+    f"{project_name}/pipeline/training_pipeline.py",
+    f"{project_name}/utils/__init__.py",
+    f"{project_name}/utils/util.py",
+    "config/config.yaml",
+    ".dockerignore",
+    "app.py",
+    "Dockerfile",
+    "setup.py",
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    file_dir, filename = os.path.split(filepath)
+
+    if file_dir != "":
+        os.makedirs(file_dir, exist_ok=True)
+        logging.info(f"Directory {file_dir} created for {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            logging.info(f"file {f} created")
+    else:
+        logging.info(f"file {f} already exist")
